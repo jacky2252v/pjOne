131050f47805f1464dec88f669951fbbcbbbacfe
jacky2252v <vivekbaraiya000@gmail.com>
2025-02-23 13:01:51 +0530

    add portfolio components and images for presentation

--- src/components/LP1.jsx
+++ src/components/LP1.jsx
@@ -5,12 +5,16 @@ const LPOne = () => {
   return (
     <div>
       <div className="flex justify-center items-center mt-32 relative">
-        <img className="opacity-88 h-[400px]" src={myImg} alt="Background" />
+        <img
+          className="opacity-88 h-[400px] -rotate-2"
+          src="/src/assets/PJ-10.jpg"
+          alt="Background"
+        />
         <h1
           className="text-[13rem] mb-[26rem] absolute z-10"
           style={{ fontFamily: "Moontime" }}
         >
-          Vivek Baraiya
+          Tristan Clousso
         </h1>
         <h1 className="text-[9rem] mt-[23rem] font-thin tracking-wider absolute z-10">
           Portfolio
